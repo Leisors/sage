60ef47ccdccbab7272b867844b17b94686b1ba83
Tom-m <tomm.manca@gmail.com>
2019-07-10 10:14:34 +0100

    base layout edits

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,23 +1,34 @@
 <!doctype html>
 <html {!! get_language_attributes() !!}>
+
   @include('partials.head')
+  
   <body @php body_class() @endphp>
+    @include('partials.gtm-body')
+
     @php do_action('get_header') @endphp
     @include('partials.header')
-    <div class="wrap container" role="document">
-      <div class="content">
+
+    <div class="wrap container" data-barba="wrapper" role="document">
+
+      <div class="content" data-barba="container" data-barba-namespace="{{ App::slug() }}">
+
         <main class="main">
           @yield('content')
         </main>
+
         @if (App\display_sidebar())
           <aside class="sidebar">
             @include('partials.sidebar')
           </aside>
         @endif
+
       </div>
     </div>
+
     @php do_action('get_footer') @endphp
     @include('partials.footer')
     @php wp_footer() @endphp
+    
   </body>
 </html>
---
+++ resources/views/partials/gtm-body.blade.php
@@ -0,0 +1,5 @@
+@if( !is_user_logged_in() )
+
+    
+
+@endif
